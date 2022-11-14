5204257df7c97705c31c109fdd6384d147263059
hyohyo0522 <117794263+hyohyo0522@users.noreply.github.com>
2022-11-14 14:05:07 +0900

    making chest

--- Assets/Scripts/ToucedObj/Chest.cs
+++ Assets/Scripts/ToucedObj/Chest.cs
@@ -6,30 +6,59 @@ using UnityEngine;
 public class Chest : MonoBehaviour,ITouchedObj
 {
 
-    private bool touchOn;
-    public Collider2D directIcon;
-    float rotateSpeed = 1f;
+
+    bool isOpen; // 상자를 오픈했는가?
+    bool touchOn; // 상자를 플레이어가 눌렀느냐
+
+
+
+    // 화살표 아이콘 관련 
+    [SerializeField] Collider2D directIcon;
+    float iconSpeed = 1f;
     [SerializeField] float MaxTimeCycle = 1f;
     float moveTimeCycle = 1f;
     bool iconmoveUp = false;
 
+    Animator chestAnimator;
 
-    
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         //iscamera = Camera.main;
         touchOn = false;
+        isOpen = false;
+        chestAnimator = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isOpen)
+        {
+            if (this.GetComponent<BoxCollider2D>().IsTouchingLayers(1 << 8)) //상자
+            {
 
+                openChest();
+            }
+
+            if (this.GetComponent<BoxCollider2D>().IsTouching(FindObjectOfType<PlayerMovement>().GetComponent<CapsuleCollider2D>())){
+                Debug.Log("플레이어가 상자에 뻥!!!");
+            }
+
+        }
+
+
+
+        // Direct Icon 움직이게 하는 코드 
         if (iconmoveUp)
         {
             moveTimeCycle -= Time.deltaTime;
-            directIcon.transform.Translate(Vector3.up * rotateSpeed * Time.deltaTime);
+            directIcon.transform.Translate(Vector3.up * iconSpeed * Time.deltaTime);
             if (moveTimeCycle<=0)
             {
                 iconmoveUp = false;
@@ -39,7 +68,7 @@ public class Chest : MonoBehaviour,ITouchedObj
         else
         {
             moveTimeCycle -= Time.deltaTime;
-            directIcon.transform.Translate(Vector3.down * rotateSpeed * Time.deltaTime);
+            directIcon.transform.Translate(Vector3.down * iconSpeed * Time.deltaTime);
             if (moveTimeCycle <= 0)
             {
                 iconmoveUp = true;
@@ -51,6 +80,11 @@ public class Chest : MonoBehaviour,ITouchedObj
     }
 
 
+    void openChest()
+    {
+        isOpen = true;
+        chestAnimator.SetBool("open", true);
+    }
 
     public void Touch()
     {
@@ -58,6 +92,8 @@ public class Chest : MonoBehaviour,ITouchedObj
         Debug.Log("상자가 클릭되었다!!!!!! 인터페이스 터치 작동!!");
         //여기에 상자 Touch 되었을 때의 동작을 넣는다.
     }
+
+
 
     //private void OnMouseDown()
     //{
